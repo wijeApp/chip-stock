59003691c43e770f156ca17fc37129e3c748c4ec
unknown <wijemobapp@gmail.com>
2025-08-07 14:25:27 +0530

    csp-01

---
+++ chipstock/src/main/java/com/tas/chipstock/ChipstockApplication.java
@@ -0,0 +1,13 @@
+package com.tas.chipstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipstockApplication.class, args);
+	}
+
+}
---
+++ chipstock/src/test/java/com/tas/chipstock/ChipstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tas.chipstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
